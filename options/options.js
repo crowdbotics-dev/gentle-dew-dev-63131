d46c97f254aa243a4b5bc6b6dde1fc2c7e014902
Crowdbotics <engineering@crowdbotics.com>
2023-02-13 03:11:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "gentle-dew-dev-63131",
+  url: "https://gentle-dew-dev-63131.botics.co",
+  api: "https://gentle-dew-dev-63131.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
